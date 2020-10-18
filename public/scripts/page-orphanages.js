bcb135b7649c67b2f9ee518a76c5f92bb81edf58
Gabriel Leandro <48261292+GabrielLeandroBS@users.noreply.github.com>
2020-10-18 14:48:45 -0300

    Update page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,6 +1,13 @@
 const map = L.map("mapid").setView([-27.2109325, -49.6448719], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2FicmllbGxlYW5kcm8iLCJhIjoiY2tnOGVzODZhMGEwMjMwcGNqcWQyeGVnOSJ9.lhshSgBVEvFvLDepG6aARg'
+}).addTo(map);
 
 const icon = L.icon({
   iconUrl: "/images/map-marker.svg",
